d8b8f6464ebe136f4434cfc807c741d219eb984d
Jun Shimura <junshimura@gmail.com>
2021-11-25 13:34:51 +0900

    プロジェクト ファイルを追加します。

---
+++ ConstructorSampe.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ConsoleUtility;
+
+namespace ConstructorSampe
+{
+    class ConstructorSampeMain
+    {
+        static void Main(string[] args)
+        {
+            Triangle triangle1 = new Triangle(5, 3.5f);
+            Console.WriteLine($"面積は{triangle1.GetSurface()}");
+            Triangle triangle2 = new Triangle(
+                InteractiveInput.InputFloat("幅："),
+                InteractiveInput.InputFloat("高さ："));
+            Console.WriteLine($"面積は{triangle2.GetSurface()}");
+        }
+    }
+}
